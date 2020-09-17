ccba6f5a164d5bfb67356721abe357f17566cf2a
jahanzebhaider <aliheider45@gmail.com>
2020-09-17 16:08:05 +0500

    Add files via upload
    first programe

---
+++ variables/name.py
@@ -0,0 +1,22 @@
+name="jahaZzeb haIder"
+# will convert name into tittle
+print(name.title())
+# will convert name into lowercase
+print(name.lower())
+# will convert name into UPPERCASE
+print(name.upper())
+
+#string concatination
+firstname="jahanzeb    haider"
+lastname="haider"
+print("programming language:\n     \t Python \n   \t c \n\t c++")
+fullname= firstname.upper() + " marium  " + lastname.lower()
+print('hello' + fullname)
+print(firstname.lstrip())
+print(firstname.strip())
+print(firstname.rstrip())
+name='albert eintsien'
+print("hello " + name + " would to learn Python")
+print("\t \t" + name.title() + " once said  A person who never made a mistake naver \n \t \t never tried new things" )
+
+
