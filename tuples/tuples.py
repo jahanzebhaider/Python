2f30b2085ba2c2cb4009be77465c07b08f664751
jahanzebhaider <aliheider45@gmail.com>
2020-10-05 00:58:54 +0500

    Add files via upload

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -9,3 +9,17 @@ for y in a:
 
 #print through indexes
 print(a[2])
+
+#4.13
+basics=('rotti','daal','chawal','chai')
+for basics in basics:
+    print(basics)
+
+#rejecting yhe change in python this will generate error
+# basics[0]='chicken'
+# print(basics)
+a="overwritng"
+print(a.upper())
+basics=('rotti','chicken','karhai','chai')
+for basics in basics:
+    print(basics)
