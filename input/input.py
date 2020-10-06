a7ea20024169a25d71c654ec2853fedf01fc6ee1
jahanzebhaider <aliheider45@gmail.com>
2020-10-06 17:06:01 +0500

    input

---
+++ input/input.py
@@ -0,0 +1,7 @@
+print('enter your first name')
+x=input()
+print('my name is ' + x)
+
+#we can also take input in this way too
+a=input('enter you last name ')
+print('my first name is ' + x +' last name is '+ a)
