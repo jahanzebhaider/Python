67b4e4f1741b6486fbd032a7f34fdacf4bb0bc91
jahanzebhaider <aliheider45@gmail.com>
2020-10-15 14:10:48 +0500

    while

---
+++ while.py
@@ -0,0 +1,19 @@
+
+#this will break the while loop when it reaches the no 3 iterantion
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+      break
+  i += 1
+print('hello world')
+
+#contine statement will start the loop again 
+
+i=5
+while(i<=10):
+    i+=1
+    if i ==8:
+        continue
+    print(i)
+  
